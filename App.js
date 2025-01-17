d5a932193104e1b5437e4d3ae9dba93eaa4108a4
BugHunter-X <askzenai@gmail.com>
2025-01-17 20:24:05 +0100

    Add App.js file

---
+++ App.js
@@ -0,0 +1,29 @@
+```javascript
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        {/* Incorrect Route causing the issue */}
+        <Route path="*" element={<NoMatch />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+function Home() {
+  return <h1>Home</h1>;
+}
+
+function About() {
+  return <h1>About</h1>;
+}
+
+function NoMatch() {
+  return <h1>404 Not Found</h1>;
+}
+export default App; 
+```
